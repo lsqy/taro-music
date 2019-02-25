570e616b2da4ee70e158feac6579086da1346dff
lsqy <lsqy93@gmail.com>
2019-02-25 22:42:38 +0800

    开展首页

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -78,11 +78,7 @@ class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+        
       </View>
     )
   }
