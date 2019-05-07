776b15f987f7b5e02620609e2967c6c3b3811315
lsqy <lsqy93@gmail.com>
2019-05-07 22:03:53 +0800

    update

--- src/pages/songDetail/index.tsx
+++ src/pages/songDetail/index.tsx
@@ -76,7 +76,6 @@ class Page extends Component<PageStateProps & PageDispatchProps, PageState> {
 
   /**
    * 指定config的类型声明为: Taro.Config
-   *
    * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
    * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
