d782bc35320b7098e1e5e667b26c1fa2cf26650d
lsqy <lsqy93@gmail.com>
2019-08-28 21:57:54 +0800

    feat(search): update

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -94,3 +94,15 @@ export const formatCount = (times) => {
   }
   return formatTime
 }
+
+// 格式化时间戳为日期
+export const formatTimeStampToTime = (timestamp) => {
+  const date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  const year = date.getFullYear();
+  const month = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  // const hour = date.getHours() + ':';
+  // const minutes = date.getMinutes() + ':';
+  // const second = date.getSeconds();
+  return `${year}-${month}-${day}`
+}
