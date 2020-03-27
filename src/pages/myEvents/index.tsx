1511f1ea0256584ad14b2c3e8689b376292465b6
lsqy <lsqy93@gmail.com>
2020-03-27 17:18:13 +0800

    refactor(events): use react-hooks

--- src/pages/myEvents/index.tsx
+++ src/pages/myEvents/index.tsx
@@ -1,59 +1,25 @@
-import { ComponentClass } from 'react'
-import Taro, { Component, Config } from '@tarojs/taro'
+import Taro, { FC, useState, useEffect } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import api from '../../services/api'
 import './index.scss'
 
-
-type PageState = {
-  userId: number
-}
-
-class Page extends Component<{}, PageState> {
-
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-  config: Config = {
-    navigationBarTitleText: '我的动态'
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      userId: Taro.getStorageSync('userId'),
-    }
-  }
-
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
-
-  componentWillMount () {
-    const { userId } = this.state
+const Page: FC = () => {
+  const [ userId ] = useState<number>(Taro.getStorageSync('userId'))
+  useEffect(() => {
     api.get('/user/event', {
       uid: userId
     }).then((res) => {
       console.log('event =>', res)
     })
-  }
-
-  componentDidShow () {
-   }
-
-  componentDidHide () { }
-
-
-  render () {
-    return (
-      <View className='template_container'>
-      </View>
-    )
-  }
+  }, [])
+  return (
+    <View className='template_container'>
+    </View>
+  )
 }
 
-export default Page as ComponentClass
+Page.config = {
+  navigationBarTitleText: '我的动态'
+}
+
+export default Page
