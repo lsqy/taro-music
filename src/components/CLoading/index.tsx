a7576c6f0fed5e1441820ce337c6fec053b65098
yiqian.lsq <yiqian.lsq@alibaba-inc.com>
2020-04-19 19:59:30 +0800

    refactor(cloading&ctitle): memo优化渲染

--- src/components/CLoading/index.tsx
+++ src/components/CLoading/index.tsx
@@ -1,24 +1,24 @@
-import Taro, { FC } from '@tarojs/taro'
-import classnames from 'classnames'
-import { View } from '@tarojs/components'
-import './index.scss'
+import Taro, { FC, memo } from "@tarojs/taro";
+import classnames from "classnames";
+import { View } from "@tarojs/components";
+import "./index.scss";
 
 type Props = {
-  fullPage?: boolean,
-  hide?: boolean
-}
+  fullPage?: boolean;
+  hide?: boolean;
+};
 
 const CLoading: FC<Props> = ({ fullPage, hide }) => {
   const cls = classnames({
-      loading_components: true,
-      fullScreen: fullPage,
-      hide: hide
-  })
-  return (
-      <View className={cls}>
-          
-      </View>
-  )
-}
+    loading_components: true,
+    fullScreen: fullPage,
+    hide: hide
+  });
+  return <View className={cls}></View>;
+};
 
-export default CLoading
+export default memo(CLoading, (oldProps, newProps) => {
+  return (
+    oldProps.fullPage === newProps.fullPage && oldProps.hide === newProps.hide
+  );
+});
--- src/components/CTitle/index.tsx
+++ src/components/CTitle/index.tsx
@@ -1,23 +1,27 @@
-import Taro, { FC } from '@tarojs/taro'
-import classnames from 'classnames'
-import { View, Image } from '@tarojs/components'
+import Taro, { FC, memo } from "@tarojs/taro";
+import classnames from "classnames";
+import { View, Image } from "@tarojs/components";
 
-import './index.scss'
+import "./index.scss";
 type Props = {
-    isFixed: boolean
-}
-
+  isFixed: boolean;
+};
 
 const CTitle: FC<Props> = ({ isFixed }) => {
   const cls = classnames({
-      title_components: true,
-      fixed: isFixed
-  })
+    title_components: true,
+    fixed: isFixed
+  });
   return (
-      <View className={cls}>
-          <Image className='title_components__logo' src={require('../../assets/images/logo.png')} />
-      </View>
-  )
-}
+    <View className={cls}>
+      <Image
+        className="title_components__logo"
+        src={require("../../assets/images/logo.png")}
+      />
+    </View>
+  );
+};
 
-export default CTitle
+export default memo(CTitle, (oldProps, newProps) => {
+  return oldProps.isFixed === newProps.isFixed;
+});
